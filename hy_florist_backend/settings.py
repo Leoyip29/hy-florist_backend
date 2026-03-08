218d5f7193214335658ff0a3a7af71d96205d9d5
ali <alimujtaba478@gmail.com>
2026-03-09 01:25:26 +0500

    white noise added

--- hy_florist_backend/settings.py
+++ hy_florist_backend/settings.py
@@ -157,7 +157,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 STATIC_URL = "/static/"
 STATIC_ROOT = BASE_DIR / "staticfiles"
-STATICFILES_DIRS = [BASE_DIR / "static"]
 
 # Media files (User uploaded content)
 MEDIA_URL = '/media/'
